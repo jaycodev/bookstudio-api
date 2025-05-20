ef13e955199b2471a0a74625687bd2456fa25ad9
jason-vila <jasonvilac@gmail.com>
2025-05-20 17:21:29 -0500

    fix: correct image URL in PDF generation for loans

--- src/main/webapp/js/loans.js
+++ src/main/webapp/js/loans.js
@@ -972,7 +972,7 @@ function generateLoanReceipt(response) {
 function generatePDF(loanTable) {
 	const { jsPDF } = window.jspdf;
 	const doc = new jsPDF("l", "mm", "a4");
-	const logoUrl = '/bookstudio-v1/images/bookstudio-logo-no-bg.png';
+	const logoUrl = '/images/bookstudio-logo-no-bg.png';
 
 	const currentDate = new Date();
 	const fecha = currentDate.toLocaleDateString('es-ES', {
