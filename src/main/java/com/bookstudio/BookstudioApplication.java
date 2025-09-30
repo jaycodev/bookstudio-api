84b4b0f78a8935cd920cef7ce89a3931c358fa76
jaycodev <jason.vilac@gmail.com>
2025-09-30 16:03:36 -0500

    📝 docs(api): add documentation configuration with springdoc-openapi

--- src/main/java/com/bookstudio/BookstudioApplication.java
+++ src/main/java/com/bookstudio/BookstudioApplication.java
@@ -2,8 +2,17 @@ package com.bookstudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title = "BookStudio API",
+        version = "1.0.0",
+        description = "REST API for a web library management platform."
+    )
+)
 public class BookstudioApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookstudioApplication.class, args);
